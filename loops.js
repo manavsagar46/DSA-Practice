be9551bb48c89c3586d0c5e1f5cd48d7e27db956
Manav sagar <manavsagar46@gmail.com>
2025-08-18 23:55:52 +0530

    Move Zeros

--- loops.js
+++ loops.js
@@ -3117,7 +3117,7 @@ console.log(reverse(-123));
 */
 
 // ? Q. Two Sum Optimized Approch
-
+/*
 var twoSum = function (nums, target) {
   let map = {};
   for (let i = 0; i < nums.length; i++) {
@@ -3132,4 +3132,25 @@ var twoSum = function (nums, target) {
   }
 
   return [];
+};
+*/
+
+// ? 283. Move Zeroes
+/*
+Given an integer array nums, move all 0's to the end of it 
+while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+*/
+
+var moveZeroes = function (nums) {
+  let x = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[x] = nums[i];
+      x++;
+    }
+  }
+  for (let i = x; i < nums.length; i++) {
+    nums[i] = 0;
+  }
 };

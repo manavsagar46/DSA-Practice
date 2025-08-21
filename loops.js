da816a07b95b229f4257e6f2e624a9ba4e2c9e55
Manav sagar <manavsagar46@gmail.com>
2025-08-22 00:05:17 +0530

    Binary Search

--- loops.js
+++ loops.js
@@ -3286,7 +3286,7 @@ console.log(findWordsContaining(words, x));
 */
 
 // ! Without Built in methods
-
+/*
 var findWordsContaining = function (words, x) {
   let res = [];
 
@@ -3305,3 +3305,42 @@ var findWordsContaining = function (words, x) {
 let words = ["abc","bcd","aaaa","cbc"];
 let x = "a";
 console.log(findWordsContaining(words, x));
+*/
+
+/*
+? 704. Binary Search
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+*/
+
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (right >= left) {
+    let middle = Math.floor((right + left) / 2);
+
+    if (target === nums[middle]) {
+      return middle;
+    } else if (target < middle) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return -1;
+};
+
+let nums = [-1, 0, 3, 5, 9, 12];
+let target = 9;
+console.log(search(nums, target));

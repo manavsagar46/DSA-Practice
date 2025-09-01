c9fd2e1f6637ace399c78809b0b4fab1be5defc2
Manav sagar <manavsagar46@gmail.com>
2025-09-02 00:12:04 +0530

    commit

--- loops.js
+++ loops.js
@@ -3445,21 +3445,36 @@ var fib = function(n) {
     return fib(n - 1) + fib(n - 2);
 };
 */
-// ? 66 PLus One:  
-var plusOne = function(digits) {
-    
-    let newArr = [...digits];
 
-    for(let i = newArr.length - 1; i >= 0; i--){
-        if(newArr[i] < 9){
-            newArr[i]++;
-            return newArr;
-        }
+// ? 66 PLus One:
+/*
+Example 2:
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
 
-        newArr[i] = 0;
+Example 3:
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+*/
+
+var plusOne = function (digits) {
+  let newArr = [...digits];
+
+  for (let i = newArr.length - 1; i >= 0; i--) {
+    if (newArr[i] < 9) {
+      newArr[i]++;
+      return newArr;
     }
-
-    newArr.unshift(1);
-
-   return newArr
+    newArr[i] = 0;
+  }
+  newArr.unshift(1);
+  return newArr;
 };
+let digits = [4, 3, 2, 1];
+console.log(plusOne(digits));

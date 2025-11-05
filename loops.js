6359fba70ed58cc58ef18cdaa301c334c8353cc0
Manav sagar <manavsagar46@gmail.com>
2025-11-06 00:04:36 +0530

    One more question

--- loops.js
+++ loops.js
@@ -3541,7 +3541,7 @@ console.log(distributeCandies([1,1,2,2,3,3]))
 */
 
 // ? 42. Trapping Rain Water
-
+/*
 var trap = function(height) {
   let left = new Array(height.length);
   let right = new Array(height.length);
@@ -3569,4 +3569,34 @@ var trap = function(height) {
 };
 
 console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
+*/
+
+// ? 231. Power of Two
+/*
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+Example 1:
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+
+Example 2:
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+
+Example 3:
+Input: n = 3
+Output: false
+*/
+
+function isPowerOfTwo(n) {
+  if (n === 1) return true;
+  else if (n < 1 || n % 2 !== 0) return false;
+  return isPowerOfTwo(n / 2);
+}
+
+console.log(isPowerOfTwo(8)); // true
+console.log(isPowerOfTwo(18)); // false
+
 
